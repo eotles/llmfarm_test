caa2cea4f853d1477740bd8af6b21245aaa8809d
Erkin Otles <eotles@gmail.com>
2023-12-13 21:18:30 -0500

    Initial Commit

---
+++ llmfarm_test/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  llmfarm_test
+//
+//  Created by Erkin Otles on 12/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ llmfarm_test/llmfarm_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  llmfarm_testApp.swift
+//  llmfarm_test
+//
+//  Created by Erkin Otles on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct llmfarm_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
